1a01200a0dc47cfa1c9f2ecc5daf4342227f6ebc
Alexander <hello@alexander.works>
2020-08-26 01:06:35 +0300

    Style adjustments for the "Send message" text field

--- QuickChat/Controllers/ChatVC.swift
+++ QuickChat/Controllers/ChatVC.swift
@@ -10,12 +10,18 @@ import UIKit
 import Firebase
 
 class ChatVC: UIViewController {
-
+    @IBOutlet weak var messageTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = K.appName
         self.navigationItem.setHidesBackButton(true, animated: true)
         
+        let messagePlaceholder = messageTextField.placeholder ?? ""
+        
+        let placeholderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.6413473887)
+        
+        messageTextField.attributedPlaceholder = NSAttributedString(string: messagePlaceholder, attributes: [NSAttributedString.Key.foregroundColor : placeholderColor])
     }
     
     @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
