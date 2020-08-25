d8d43bd3562228eb0ea56dd4078351678893714a
Alexander <hello@alexander.works>
2020-08-25 20:11:23 +0300

    Added log in logic

--- QuickChat/Controllers/LoginVC.swift
+++ QuickChat/Controllers/LoginVC.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginVC: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Log In"
     }
-
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+                
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    self?.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
+    }
+    
 }
