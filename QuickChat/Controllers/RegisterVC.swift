b7a2dd9941550875d680888c22283a300a7268ec
Alexander <hello@alexander.works>
2020-08-25 18:32:05 +0300

    Installed and set up Firebase. Implemented basic functionality of the Register page.

--- QuickChat/Controllers/RegisterVC.swift
+++ QuickChat/Controllers/RegisterVC.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterVC: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Register"
     }
-
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e)
+                } else {
+                    //Navigate to the ChatVC
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
+    }
 }
